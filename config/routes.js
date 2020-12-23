b4c23cc5ab7c57f77b5702dc8060c731aff790f7
OUEDRAOGO WENDZOODO FABRICE GHISLAIN <fabioued@yahoo.fr>
2020-12-23 18:50:28 +0100

    feat(routes):defining all the routes

--- config/routes.js
+++ config/routes.js
@@ -20,18 +20,38 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': { view: 'pages/homepage' },
+  '/login': { view: 'login' },
+  '/register': { view: 'register' },
+  '/rides/search': { view: 'rides/search' },
+  '/rides/price': { view: 'rides/getPrice' },
+  '/rides/time': { view: 'rides/getTime' },
+  '/rides/map': { view: 'rides/map' },
+
+  //BACKEND
+
+  //AUTHENTIFICATION
+  'POST /register': 'UsersController.register',
+  'POST /login': 'UsersController.login',
+
+  'POST /rides/search': 'RidesController.search',
+  'POST /rides/price': 'RidesController.getPrices',
+  'POST /rides/time': 'RidesController.getTime',
 
 
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+
+
+
+  'get /user/new': 'UserController.new',
+  'post /user/register': 'UserController.register',
+
+  'get /session/new': 'SessionController.new',
+  'post /session/create': 'SessionController.create',
+
+
+
+
+  '/course/waitingTimes': { view: 'coursewaitingtimes' },
+
 
 
 };
